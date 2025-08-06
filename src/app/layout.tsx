fd1ae46f82ad1c21f74e42e93544ec5210ef8e22
Frida Mwende <wawirafrida20@gmail.com>
2025-08-06 13:03:13 +0300

    chore: set up Redux Toolkit store with TypeScript and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "../../styles/globals.css";
 import Navbar from "../../components/Header";
 import Footer from "../../components/Footer";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 
 export const metadata = {
   title: "CrownVote - Miss Kenya Pageant Polls",
@@ -10,13 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body 
-        className="min-h-screen flex flex-col font-sans"
-        suppressHydrationWarning={true}
-      >
-        <Navbar />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+      <body className="min-h-screen flex flex-col font-sans" suppressHydrationWarning={true}>
+        <Provider store={store}>
+          <Navbar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
